dd497b7585093ac1c8e22a5a5d2e71a3a47fe8b2
Bolduratu <bolduratun@gmail.com>
2022-01-18 10:14:41 +0200

    prm commit

---
+++ n1.php
@@ -0,0 +1,40 @@
+<?php
+echo "hello world";
+
+$a=10;
+$b=2;
+
+echo '<br>a='.$a;
+echo '<br>b='.$b;
+echo '<br>Сумма='. $a+$b;
+echo '<br>Разность='.$a-$b;
+echo '<br>Произведение='.$a*$b;
+echo '<br>Частное='.$a/$b.'<br>';
+
+$c=15;
+$d=2;
+echo '<br>c='.$c;
+echo '<br>d='.$d;
+$result=$c+$d;
+echo '<br>Результат='.$result.'<br>';
+
+$a=10;
+$b=2;
+$с=5;
+echo '<br>a='.$a;
+echo '<br>b='.$b;
+echo '<br>c='.$c;
+echo '<br>Сумма='. $a+$b+$c.'<br>';
+
+
+$a=17;
+$b=10;
+echo '<br>a='.$a;
+echo '<br>b='.$b;
+$с=$a-$b;
+echo '<br>с='.$с;
+$d=7;
+$result=$c+$d;
+echo '<br>Результат='.$result;
+
+?>
